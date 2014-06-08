ccbaaff74d4c0540b06b8d37d6a3b443badcc806
Pavel Voronin <pavel.a.voronin@gmail.com>
2014-06-09 00:11:21 +0400

    add some links for members

--- protected/views/common/footer.php
+++ protected/views/common/footer.php
@@ -1,13 +1,23 @@
+<?php if(Yii::app()->user->isGuest): ?>
 <footer>
+<?php endif ?>
 	<div class="row">
 		<div class="large-12 columns">
 			<hr />
 			<ul class="inline-list ">
 				<li><a href="<?php echo $this->createUrl('static/page', ['view' => 'about']) ?>">About Us</a></li>
 				<li><a href="<?php echo $this->createUrl('static/page', ['view' => 'api']) ?>">API</a></li>  
+				<?php if(!Yii::app()->user->isGuest): ?>
+				<li><a href="<?php echo $this->createUrl('index/key') ?>">Key</a></li>  
+				<?php endif ?>
 				<li><a href="<?php echo $this->createUrl('static/page', ['view' => 'apps']) ?>">Bookmarklet</a></li>
 				<li><a href="<?php echo $this->createUrl('static/page', ['view' => 'faq']) ?>">FAQ</a></li>
+				<?php if(!Yii::app()->user->isGuest): ?>
+				<li><a href="<?php echo $this->createUrl('auth/logout') ?>">Logout</a></li>  
+				<?php endif ?>
 			</ul>
 		</div>
 	</div>
+<?php if(Yii::app()->user->isGuest): ?>
 </footer>
+<?php endif ?>
