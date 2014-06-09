9b40f7c96cba9d0f437e23d57f81b281056d572a
Pavel Voronin <pavel.a.voronin@gmail.com>
2014-06-09 13:42:11 +0400

    fix cookies for submomains

--- html/index.php
+++ html/index.php
@@ -3,6 +3,7 @@
 ini_set('display_errors', true);
 error_reporting(E_ALL | E_NOTICE | E_STRICT);
 unset($_SERVER['HTTP_HOST']);
+ini_set('session.cookie_domain', '.' . $_SERVER['SERVER_NAME']);
 
 $yii = dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'framework' . DIRECTORY_SEPARATOR . 'yii.php';
 $config = dirname(__FILE__) . DIRECTORY_SEPARATOR . '..'. DIRECTORY_SEPARATOR . 'protected'. DIRECTORY_SEPARATOR . 'config'. DIRECTORY_SEPARATOR . 'main.php';
